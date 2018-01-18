513e8110b27ab1339e47c45888e35d90777cfdcc
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-01-18 13:57:06 +0100

    Updates Node. Updates dependencies (patch)

--- test/modules/dev-dependencies-test.js
+++ test/modules/dev-dependencies-test.js
@@ -1,9 +1,7 @@
-'use strict'
-
 const test = require('ava')
 const pkg = require('../../package.json')
 const dependencies = pkg.devDependencies || {}
-const dropModules = ['micro-dev']
+const dropModules = ['micro-dev', 'nsp']
 const isDropped = (module) => !dropModules.includes(module)
 
 Object.keys(dependencies).filter(isDropped).forEach((dependency) => {
